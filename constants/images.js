1f83baa917a01d68175a8e2fbe507848547f9e9d
Biplab Pati <droploot8@gmail.com>
2023-05-23 11:47:03 +0530

    app build andriod

--- constants/images.js
+++ constants/images.js
@@ -4,16 +4,12 @@ import home from "../assets/images/home.png";
 import loading from "../assets/images/loading.gif";
 import logout from "../assets/images/logout.png";
 import AuthImg from "../assets/images/AuthImg.png";
-import googleIcon from "../assets/images/googleIcon.jpg";
-import googleIconPng from "../assets/images/googleIcon.png";
 
 
 export default {
   profile,
   logout,
   AuthImg,
-  googleIcon,
-  googleIconPng,
   chat,
   home,
   loading
